b8fb9b1df2ba051b2be48ef97603c487e1f1f339
andreea <mariaandreealavric@gmail.com>
2024-07-22 17:53:10 +0200

    aggiustato il main activity, aggiunto il myapplication.kt, aggiustato il androidmanifest, e build.grandle di app

--- android/app/src/main/kotlin/io/andreealavric/fingerfy/MainActivity.kt
+++ android/app/src/main/kotlin/io/andreealavric/fingerfy/MainActivity.kt
@@ -1,5 +1,10 @@
 package io.andreealavric.fingerfy
-
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Eventuali operazioni specifiche per l'attività
+    }
+}
